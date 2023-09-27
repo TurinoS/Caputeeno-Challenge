959d0af54e4df9b35a9cec033789136a861d5db1
Paulo Turino <paulo.turino01@gmail.com>
2023-09-27 10:10:23 -0300

    feat: Introduce product sorting list in filter component for enhanced product display

--- caputeeno/src/components/Filter.tsx
+++ caputeeno/src/components/Filter.tsx
@@ -3,13 +3,14 @@
 import styled from 'styled-components';
 import arrowDown from '../../public/Arrow.png';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const StyledFilter = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 2em 0;
 
-    & ul {
+    &.filter {
         list-style: none;
         display: flex;
         gap: 2.5em;
@@ -35,18 +36,34 @@ const StyledFilter = styled.div`
         cursor: pointer;
         background-color: var(--gray)
     }
+
+    &.list {
+        display:flex;
+        flex-direction: column;
+        position: relative;
+    }
 `
 
 export default function Filter() {
+    const [openFilter, setOpenFilter] = useState(false)
+
     return(
         <StyledFilter>
-            <ul>
+            <ul className='filter'>
                 <li className='selected'>TODOS OS PRODUTOS</li>
                 <li>CAMISETAS</li>
                 <li>CANECAS</li>
             </ul>
             <div>
-                <button>Organizar por <Image src={arrowDown} alt='Open list' width={30} /></button>
+                <button>Organizar por <Image src={arrowDown} alt='Open list' width={30} onClick={() => setOpenFilter(!openFilter)} /></button>
+                {openFilter &&
+                    <ul className='list'>
+                        <li>Novidades</li>
+                        <li>Preço: Maior - menor</li>
+                        <li>Preço: Menor - maior</li>
+                        <li>Mais vendidos</li>
+                    </ul>
+                }
             </div>
         </StyledFilter>
     )
