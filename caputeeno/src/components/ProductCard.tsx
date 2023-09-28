d74fb7de37e2236e4b98920899df3542315bd094
Paulo Turino <paulo.turino01@gmail.com>
2023-09-28 11:01:16 -0300

    style: Finalize styling for store catalog page

--- caputeeno/src/components/ProductCard.tsx
+++ caputeeno/src/components/ProductCard.tsx
@@ -1,25 +1,46 @@
-'use client'
+"use client";
 
-import Image from 'next/image';
-import styled from 'styled-components';
+import Image from "next/image";
+import styled from "styled-components";
 
 interface CardProps {
-    name: string,
-    src: string,
-    price: number,
+  name: string;
+  src: string;
+  price: number;
 }
 
 const Card = styled.div`
-    border-radius: 8px 8px 0 0;
-`
+  overflow: hidden;
+  border-radius: 8px 8px 0 0;
+  width: fit-content;
+
+  &:hover {
+    scale: 1.05;
+    transition: 450ms ease-in-out;
+  }
+
+  & h4 {
+    font-weight: 300;
+    padding: 2px 12px 6px;
+    border-bottom: 1px solid var(--gray);
+    line-height: 24px;
+  }
+
+  & p {
+    font-size: 14px;
+    font-weight: 600;
+    color: var(--black);
+    padding: 6px 12px;
+    line-height: 21px;
+  }
+`;
 
 export default function ProductCard({ name, src, price }: CardProps) {
-    return(
-       <Card>
-            <Image src={src} alt={name} width={350} height={350} />
-            <p>{name}</p>
-            <p>{price}</p>
-        </Card> 
-    )
-    
-} 
+  return (
+    <Card>
+      <Image src={src} alt={name} width={240} height={250} />
+      <h4>{name}</h4>
+      <p>R${price / 100}</p>
+    </Card>
+  );
+}
--- caputeeno/src/components/ProductList.tsx
+++ caputeeno/src/components/ProductList.tsx
@@ -1,15 +1,25 @@
 import { useProducts } from '@/hooks/useProducts'
 import ProductCard from './ProductCard';
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+    display: flex;
+    column-gap: 2em;
+    row-gap: 1.5em;
+    padding-top: 2em;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
 
 export default function ProductList() {
     const { data } = useProducts();
     console.log(data);
 
     return(
-        <div>
+        <StyledSection>
             {data?.map((product) => (
                 <ProductCard name={product.name} src={product.image_url} price={product.price_in_cents} key={product.id} />
             ))}
-        </div>
+        </StyledSection>
     )
 }
